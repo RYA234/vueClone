d1227add76d6da19b7f0896aee6cc6e02d7b62e1
RYA234 <mryuabc@gmail.com>
2023-09-14 07:36:44 +0900

    37. Adding new feature to mark the task as completed

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,7 +1,12 @@
 <template>
     <li class="list-group-item py-3">
         <div class="d-flex justify-content-start align-items-center">
-            <input class="form-check-input mt-0" :class="completedClass" type="checkbox" :checked="task.is_completed" />
+            <input class="form-check-input mt-0" 
+                :class="completedClass" 
+                type="checkbox" 
+                :checked="task.is_completed" 
+                @change="markTaskAsCompleted"
+                />
             <div class="ms-2 flex-grow-1" 
                 :class="completedClass" 
                 title="Double click the text to edit or remove"
@@ -32,7 +37,7 @@ const props = defineProps({
     task: Object
 })
 
-const emit = defineEmits(['updated'])
+const emit = defineEmits(['updated', 'completed'])
 const isEdit = ref(false)
 const editingTask = ref(props.task.name)
 const completedClass = computed(() => props.task.is_completed ? "completed" : "")
@@ -49,5 +54,10 @@ const updateTask = event =>{
     const updatedTask ={...props.task, name: event.target.value}
     isEdit.value = false
     emit('updated', updatedTask)
+}
+
+const markTaskAsCompleted = () =>{
+    const updatedTask = {...props.task, is_completed: !props.task.is_completed}
+    emit('completed', updatedTask)
 }
 </script>
